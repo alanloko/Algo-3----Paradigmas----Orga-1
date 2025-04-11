6a6bd4df40eea26611c3111690fa93915163a0d6
caruarte <caruarte@gmail.com>
2025-04-11 03:41:52 -0300

    Le puse nombre a los tests que faltaban.

--- Paradigmas/Tp Paradigmas/src/TestsPicoyPala.hs
+++ Paradigmas/Tp Paradigmas/src/TestsPicoyPala.hs
@@ -41,27 +41,35 @@ testsEj2 =
       "string vacio string" ~: texto "a" <+> vacio <+> texto "b" ~?= texto "ab",
       "vacio string vacio" ~: vacio <+> texto "a" <+> vacio ~?= texto "a",
       "concatenar indentar" ~: texto "a" <+> indentar 2 linea <+> texto "b" ~?= indentar 2 (texto "a" <+> linea <+> texto "b"),
-      "vacio con linea" ~: vacio <+> linea ~?= linea
+      "vacio con linea" ~: vacio <+> linea ~?= linea,
+      "tres strings" ~: texto "a" <+> texto "b" <+> texto "c" ~?= texto "abc"
     ]
---testeamos concatenar de formas simples y por ultimo probar multiples concatenaciones, sumado a concatenar vacios para verificar que se mantenga el doc.
+-- testeamos concatenar de formas simples y por ultimo probar multiples concatenaciones, sumado a concatenar vacios para verificar que se mantenga el doc.
 
 testsEj3 :: Test
 testsEj3 =
   test
-    [ indentar 0 (texto "sin cambio") ~?= texto "sin cambio",
-      indentar 3 (texto "a" <+> linea) ~?= texto "a" <+> indentar 3 linea
+    [ "indentar 0 con texto" ~: indentar 0 (texto "sin cambio") ~?= texto "sin cambio",
+      "indentar texto y linea" ~: indentar 3 (texto "a" <+> linea) ~?= texto "a" <+> indentar 3 linea,
+      "indentar 2 lineas" ~: indentar 2 (texto "a" <+> linea <+> texto "b" <+> linea) ~?= texto "a" <+> indentar 2 linea <+> texto "b" <+> indentar 2 linea,
+      "indentar vacio" ~: indentar 2 vacio ~?= vacio,
+      "indentar nro negativo" ~: indentar (-3) (texto "a" <+> linea) ~?= texto "a" <+> linea
     ]
---testeamos el funcionamiento basico de indentar, y un test un poco más complejo para verificar el resultado de una asociacion distinta.
+-- testeamos el funcionamiento basico de indentar
 
 testsEj4 :: Test
 testsEj4 =
   test
-    [ mostrar (texto "hola") ~?= "hola",
-      mostrar (linea <+> texto "fin") ~?= "\nfin",
-      mostrar (indentar 1 (linea <+> texto "x")) ~?= "\n x",
-      mostrar (texto "a" <+> linea <+> texto "b" <+> linea <+> texto "c") ~?= "a\nb\nc"
+    [ "mostrar texto" ~: mostrar (texto "hola") ~?= "hola",
+      "mostrar linea con texto" ~: mostrar (linea <+> texto "fin") ~?= "\nfin",
+      "mostrar linea con indentado y texto" ~: mostrar (indentar 1 (linea <+> texto "x")) ~?= "\n x",
+      "mostrar varias lineas" ~: mostrar (texto "a" <+> linea <+> texto "b" <+> linea <+> texto "c") ~?= "a\nb\nc",
+      "mostrar vacio" ~: mostrar vacio ~?= "",
+      "mostrar indentado con espacios" ~: mostrar (indentar 3 (texto "a" <+> linea <+> texto "x")) ~?= "a\n   x"
     ]
---testeamos que mostrar use correctamente el ingreso de una linea.
+-- testeamos que mostrar use transforme correctamente un Doc a un String.
+
+-- ejemplos de ppones
 pericles, merlina, addams, familias, pedro :: PPON
 pericles = ObjetoPP [("nombre", TextoPP "Pericles"), ("edad", IntPP 30)]
 merlina = ObjetoPP [("nombre", TextoPP "Merlina"), ("edad", IntPP 24)]
@@ -72,24 +80,26 @@ pedro = ObjetoPP [("0", addams),("1", addams),("2", merlina)]
 testsEj5 :: Test
 testsEj5 =
   test
-    [ pponAtomico (TextoPP "hola") ~?= True,
-      pponAtomico (IntPP 123) ~?= True,
-      pponAtomico (ObjetoPP []) ~?= False,
-      pponAtomico (ObjetoPP [("x", IntPP 1)]) ~?= False,
-      pponAtomico merlina ~?= False
+    [ "pponAtomico Texto" ~: pponAtomico (TextoPP "hola") ~?= True,
+      "pponAtomico Int" ~: pponAtomico (IntPP 123) ~?= True,
+      "pponAtomico ObjetoPP Vacio" ~: pponAtomico (ObjetoPP []) ~?= False,
+      "pponAtomico ObjetoPP Simple" ~: pponAtomico (ObjetoPP [("x", IntPP 1)]) ~?= False,
+      "pponAtomico ObjetoPP Compuesto" ~: pponAtomico addams ~?= False,
+      "pponAtomico ObjetoPP Simple 2" ~: pponAtomico merlina ~?= False
     ]
---agregamos el test 5 para ver el correcto uso de pponAtomico, con datos guardados en memoria y con otros ingresados en el momento.
-
+-- testeamos el funcionamiento de pponAtomico, con datos guardados en memoria y con otros ingresados en el momento.
 
 testsEj6 :: Test
 testsEj6 =
   test
-    [ pponObjetoSimple (ObjetoPP []) ~?= True,
-      pponObjetoSimple (ObjetoPP [("x", IntPP 42), ("y", TextoPP "z")]) ~?= True,
-      pponObjetoSimple (ObjetoPP [("a", TextoPP "b"), ("b", ObjetoPP [])]) ~?= False,
-      pponObjetoSimple (ObjetoPP [("a", IntPP 1), ("b", ObjetoPP [("c", TextoPP "d")])]) ~?= False
+    [ "pponObjetoSimple Objeto Vacio" ~: pponObjetoSimple (ObjetoPP []) ~?= True,
+      "pponObjetoSimple Texto" ~: pponObjetoSimple (TextoPP "hola") ~?= False,
+      "pponObjetoSimple Int" ~: pponObjetoSimple (IntPP 123) ~?= False,
+      "pponObjetoSimple dos tuplas" ~: pponObjetoSimple (ObjetoPP [("x", IntPP 42), ("y", TextoPP "z")]) ~?= True,
+      "pponObjetoSimple Objeto complejo con vacio" ~: pponObjetoSimple (ObjetoPP [("a", TextoPP "b"), ("b", ObjetoPP [])]) ~?= False,
+      "pponObjetoSimple Objeto Complejo" ~: pponObjetoSimple (ObjetoPP [("a", IntPP 1), ("b", ObjetoPP [("c", TextoPP "d")])]) ~?= False
     ]
---testeamos casos tanto verdaderos como falsos, uno muy simple como el de un objeto vacio y los demás un poco más complejos, usando un objeto vacio dentro de otro
+-- testeamos el funcionamiento de pponObjetoSimple con los diferentes constructores de PPON y posibles valores que puede tener.
 
 a, b, c :: Doc
 a = texto "a"
@@ -99,26 +109,32 @@ c = texto "c"
 testsEj7 :: Test
 testsEj7 =
   test
-    [ mostrar (intercalar (texto "|") [a]) ~?= "a",
-      mostrar (intercalar (linea <+> texto "-") [a, b]) ~?= "a\n-b",
-      mostrar (intercalar (texto " ") []) ~?= "",
-      mostrar (intercalar (texto " y ") [a, b, c]) ~?= "a y b y c"
+    [ "intercalar un elemento" ~: mostrar (intercalar (texto "|") [a]) ~?= "a",
+      "intercalar dos elementos" ~: mostrar (intercalar (linea <+> texto "-") [a, b]) ~?= "a\n-b",
+      "intercalar vacio" ~: mostrar (intercalar (texto " ") []) ~?= "",
+      "intercalar tres elementos" ~: mostrar (intercalar (texto " y ") [a, b, c]) ~?= "a y b y c",
+      "intercalar sep vacio" ~: mostrar (intercalar vacio [a, b, c]) ~?= "abc"
     ]
+-- testeamos el funcionamiento de intercalar con varias listas de docs y separadores
 
 testsEj8 :: Test
 testsEj8 =
   test
-    [ mostrar (aplanar (linea <+> b)) ~?= " b",
-      mostrar (aplanar (indentar 4 (a <+> linea <+> b))) ~?= "a b",
-      mostrar (aplanar (a <+> linea <+> texto "" <+> linea <+> c)) ~?= "a  c"
+    [ "aplanar linea y texto" ~: mostrar (aplanar (linea <+> b)) ~?= " b",
+      "aplanar linea con indentacion" ~: mostrar (aplanar (indentar 4 (a <+> linea <+> b))) ~?= "a b",
+      "aplanar dos lineas" ~: mostrar (aplanar (a <+> linea <+> vacio <+> linea <+> c)) ~?= "a  c",
+      "aplanar solo texto" ~: mostrar (aplanar c) ~?= "c",
+      "aplanar vacio" ~: mostrar (aplanar vacio) ~?= ""
     ]
+-- testeamos el funcionamiento de aplanar con varios tipos de documentos
 
 testsEj9 :: Test
 testsEj9 =
   test
-    [ mostrar (pponADoc (TextoPP "hola")) ~?= "\"hola\"",
-      mostrar (pponADoc (IntPP 42)) ~?= "42",
-      mostrar (pponADoc (ObjetoPP [])) ~?= "{ }",
-      mostrar (pponADoc (ObjetoPP [("a", TextoPP "b")])) ~?= "{ \"a\": \"b\" }",
-      mostrar (pponADoc pedro) ~?= "{\n  \"0\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  },\n  \"1\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  },\n  \"2\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n}"
+    [ "pponADoc texto" ~: mostrar (pponADoc (TextoPP "hola")) ~?= "\"hola\"",
+      "pponADoc int" ~: mostrar (pponADoc (IntPP 42)) ~?= "42",
+      "pponADoc Objeto Vacio" ~: mostrar (pponADoc (ObjetoPP [])) ~?= "{ }",
+      "pponADoc Objeto Simple" ~: mostrar (pponADoc (ObjetoPP [("a", TextoPP "b")])) ~?= "{ \"a\": \"b\" }",
+      "pponADoc Objeto Compuesto" ~: mostrar (pponADoc pedro) ~?= "{\n  \"0\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  },\n  \"1\": {\n    \"0\": { \"nombre\": \"Pericles\", \"edad\": 30 },\n    \"1\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n  },\n  \"2\": { \"nombre\": \"Merlina\", \"edad\": 24 }\n}"
     ]
+-- testeamos el funcionamiento de pponADoc con los distintos constructores de PPPONes
